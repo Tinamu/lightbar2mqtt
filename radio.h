3976610f09726939e30de46341f10765a3199364
Marek Benovič <38257504+Wr1nGl@users.noreply.github.com>
2026-03-08 18:11:15 +0100

    fixed functionality for 'new' monitor bars + one small bug fix (#18)
    * fixed functionality for 'new' monitor bars + one small bug fix
    
    * added state update to HA, button reset in case of state missmatch

--- radio.h
+++ radio.h
@@ -12,6 +12,7 @@ class Remote;
 struct PackageIdForSerial
 {
     uint32_t serial;
+    boolean never_read = true;
     uint8_t package_id;
 };
 
@@ -35,7 +36,8 @@ private:
     Remote *remotes[constants::MAX_REMOTES];
     uint8_t num_remotes = 0;
 
-    static const uint64_t address = 0xAAAAAAAAAAAA;
+    static const uint64_t send_address = 0x5555555555;
+    static const uint64_t receive_address = 0xAAAAAAAAAA;
     static constexpr byte preamble[8] = {0x53, 0x39, 0x14, 0xDD, 0x1C, 0x49, 0x34, 0x12};
 
     // For details on how these parameters were chosen, see
